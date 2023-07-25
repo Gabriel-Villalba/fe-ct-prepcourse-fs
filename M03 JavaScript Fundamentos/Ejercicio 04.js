cfec19cf4e7716425cd9dfbc053e7f9be127cbfe
Gabriel-Villalba <105971696+Gabriel-Villalba@users.noreply.github.com>
2023-07-25 06:11:51 -0300

    M03 terminado

--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -6,31 +6,43 @@
 function elevarAlCuadrado(num) {
    // Retorna el valor de "num" elevado al cuadrado.
    // Tu código:
+   let cuadrado = num ** 2
+   return cuadrado
 }
 
 function elevarAlCubo(num) {
    // Retorna el valor de "num" elevado al cubo.
    // Tu código:
+   let cubo = num ** 3
+   return cubo
 }
 
 function elevar(num, exponent) {
    // Retorna el valor de "num" elevado al exponente "exponent".
    // Tu código:
+   let numElevado = Math.pow(num, exponent)
+   return numElevado;
 }
 
 function redondearNumero(num) {
    // Redondea "num" al entero más próximo y retórnalo.
    // Tu código:
+   let redondeo = Math.round(num);
+		return redondeo 
+
 }
 
 function redondearHaciaArriba(num) {
    // Redondea "num" hacia arriba y retórnalo.
    // Tu código:
+   num = Math.ceil(num)
+   return num
 }
 
 function numeroRandom() {
    // Genera un número al azar entre 0 y 1 y retórnalo.
    // Tu código:
+   return Math.random()
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
